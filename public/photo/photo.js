c56b8d5f1265da058ba1482ca1dc693dc55acc3e
Joanne Wang <ennaoj.w@gmail.com>
2017-05-22 19:40:38 -0700

    kj

--- public/photo/photo.js
+++ public/photo/photo.js
@@ -31,20 +31,19 @@ function changeTags(e) {
 		var labelArea = e.target.parentElement.parentElement.parentElement.getElementsByClassName("labelArea")[0];
 
 		if(labelArea.getElementsByClassName("inputLabel")[0].style.display == "block") {
-			console.log("inif");
 			labelArea.getElementsByClassName("labelBox")[0].style.backgroundColor = "white";
 			var labels = labelArea.getElementsByClassName("labelBox")[0].getElementsByClassName("labelItem");
-			for(int i = 0; i < labels.length; i++) {
-				labels[i].getElementsByClassName("deleteLabel")[0].style.display = "none"; // BLOCK??
-			}
+			// for(int i = 0; i < labels.length; i++) {
+			// 	labels[i].getElementsByClassName("deleteLabel")[0].style.display = "none"; // BLOCK??
+			// }
 			labelArea.getElementsByClassName("inputLabel")[0].style.display = "none";
 			labelArea.getElementsByClassName("addLabelButton")[0].style.display = "none";
 		} else {
 			labelArea.getElementsByClassName("labelBox")[0].style.backgroundColor = "#C1AB9E";
 			var labels = labelArea.getElementsByClassName("labelBox")[0].getElementsByClassName("labelItem");
-			for(int i = 0; i < labels.length; i++) {
-				labels[i].getElementsByClassName("deleteLabel")[0].style.display = block; // BLOCK??
-			}
+			// for(int i = 0; i < labels.length; i++) {
+			// 	labels[i].getElementsByClassName("deleteLabel")[0].style.display = block; // BLOCK??
+			// }
 			labelArea.getElementsByClassName("inputLabel")[0].style.display = "block";
 			labelArea.getElementsByClassName("addLabelButton")[0].style.display = "block";
 		}
