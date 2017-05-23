21cc863d8406cd45e35ca71fd43ec0dd952f7535
Joanne Wang <ennaoj.w@gmail.com>
2017-05-23 00:58:03 -0700

    kj

--- public/photo/photo.js
+++ public/photo/photo.js
@@ -150,6 +150,7 @@ function enterFilter() {
 	var url = "http://138.68.25.50:10305/query?keyword="+filter+"&op=filter";
 	function reqListener() {
 		var data = this.responseText;
+		alert(data);
 		alert(data[0].fileName);
 		showPhotos(this.responseText);
 	}
