db6bf700bf4511bdb504e456738e26d649bbc8cf
Joanne Wang <ennaoj.w@gmail.com>
2017-05-22 20:18:24 -0700

    kj

--- public/photo/photo.js
+++ public/photo/photo.js
@@ -33,6 +33,7 @@ function changeTags(e) {
 		if(labelArea.getElementsByClassName("inputLabel")[0].style.display == "block") {
 			labelArea.getElementsByClassName("labelBox")[0].style.backgroundColor = "white";
 			var labels = labelArea.firstChild.getElementsByClassName("labelItem");
+			console.log(labels);
 			if(labels) {
 				for(int i = 0; i < labels.length; i++) {
 					labels[i].getElementsByClassName("deleteLabel")[0].style.display = "none"; // BLOCK??
